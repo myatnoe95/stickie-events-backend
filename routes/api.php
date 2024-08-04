bf8c23a1b45a631fb854cef764fbf9b9c050f791
myatnoe95 <khingsunthwe@gmail.com>
2024-08-04 23:46:20 +0700

    feat: add routes for controllers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PhotoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +30,14 @@ Route::middleware('auth:api')->group(function(){
     Route::post('/users/index', [UserController::class, 'index']);
     Route::apiResource('/users', UserController::class)->except(['index']);
 
+    Route::post('/events/index', [EventController::class, 'index']);
+    Route::apiResource('/events', EventController::class)->except(['index']);
+
+    Route::post('/event-photos/index', [PhotoController::class, 'index']);
+    Route::get('/event-photos', [PhotoController::class, 'getEventPhotos']); // Use this route to get all event photos by event id and created by user id
+    Route::apiResource('/event-photos', PhotoController::class)->except(['index']);
+
     Route::post('/logout',[UserController::class, 'logout']);
 });
 
+    
